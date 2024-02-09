95779d580366e69815e1cddb91817341c252f0ad
StephanieV7 <88780186+StephanieV7@users.noreply.github.com>
2024-02-09 18:52:47 +0200

    Added paint brush animation to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,13 @@ import { slides } from "../../data/carouselData.json";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.home}>
+      <div className={styles.mainContainer}>
+         <img src="src/assets/hiddenImage.jpg" alt="rose-photo" className={styles.hiddenImg}/>
+         <div className={styles.coverBlock}></div>
+         <img src="src/assets/paint-brush.png" alt="paint-brush-graphic" className={styles.brushImg}/>
+      </div>
       <Carousel data={slides} />
-    </>
+    </div>
   );
 }
